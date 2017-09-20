7b0b586ddf1ba1d33dcf727c3f70faf9ec282fa0
watkinso <watkinso@hansaluftbild.de>
2017-09-20 15:40:04 +0200

    slider css

--- src/js/pages/IcebergChartsPage.js
+++ src/js/pages/IcebergChartsPage.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 import Swiper from "react-id-swiper"
-
+import Slider from "react-slick"
 
 import "./style.less"
 
@@ -10,39 +10,73 @@ import c2 from "./ichartsImages/grid1.png"
 import c3 from "./ichartsImages/grid2_graphpaper.png"
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 export default class IcebergChartsPage extends React.Component {
+
+
+
   render() {
 
-    let w = "300px"
-    let h = "300px"
-    let bwh = "300px 300px"
-    // alert("c1 " + c1)
+    let styleSlide = {};
+    let styleSlideContainer = {};
+
+    let maxWidth = "100%";
+    let maxHeight = "100%";
+
+
+    let w = "100px"
+    let h = "600px"
+    // let bwh = w + " " + h;
+
+    let styleSize = {width:w, height:h};
+    let styleMax = {maxHeight:maxHeight, maxWidth:maxWidth};
+    let styleBorder = {border :'1px solid green'};
+    let bgImageStyle = {backgroundSize: '80% 80%', backgroundPosition: 'center', backgroundRepeat: 'no-repeat'};
+
+
+
+    styleSlide = Object.assign({}, styleSlide, styleSize);
+    styleSlide = Object.assign({}, styleSlide, styleMax);
+    styleSlide = Object.assign({}, styleSlide, styleBorder);
+    styleSlide = Object.assign({}, styleSlide, bgImageStyle);
+
+    styleSlideContainer = Object.assign({}, styleSlideContainer, styleSize);
+
     //
     //
-    // let style = {
-    //   backgroundImage: "url(" + c1 + ")",
-    //   width:"300px",
-    //   height:"200px"
-    // }
+    // let s1 = Object.assign({}, styleSlide, {backgroundImage:"url(" + c1 + ")"});
+    // let s2 = Object.assign({}, styleSlide, {backgroundImage:"url(" + c2 + ")"});
+    // let s3 = Object.assign({}, styleSlide, {backgroundImage:"url(" + c3 + ")"});
+
+
+    var settings = {
+      dots: true,
+      infinite: true,
+      speed: 500,
+      slidesToShow: 1,
+      slidesToScroll: 1
+    };
 
     return (
       <div>
-        Iceberg charts
-        <Swiper className="swiper-container">
-
-          <div className = "swiper-slide" style={{backgroundImage:"url(" + c1 + ")"}}></div>
-          <div className = "swiper-slide" style={{backgroundImage:"url(" + c2 + ")"}}></div>
-          <div className = "swiper-slide" style={{backgroundImage:"url(" + c2 + ")"}}></div>
-          <div className = "swiper-slide" style={{backgroundImage:"url(" + c2 + ")"}}></div>
-
-          <div style={{backgroundImage:"url(" + c2 + ")",  width:w, height:h, backgroundSize: {bwh}}}></div>
-          <div style={{backgroundImage:"url(" + c3 + ")",  width:w, height:h, backgroundSize: {bwh}}}></div>
-
-          <div style={{backgroundImage:"url(" + c1 + ")",  width:w, height:h, backgroundSize: {bwh}}}></div>
-          <div style={{backgroundImage:"url(" + c2 + ")",  width:w, height:h, backgroundSize: {bwh}}}></div>
-          <div style={{backgroundImage:"url(" + c3 + ")",  width:w, height:h, backgroundSize: {bwh}}}></div>
-
-        </Swiper>
+        <Slider  {...settings} style={styleSlideContainer}>
+          <div style={{ ...styleSlide, backgroundImage: `url(${c1})` }}></div>
+          <div style={{ ...styleSlide, backgroundImage: `url(${c2})` }}></div>
+        </Slider>
       </div>
     );
   }
