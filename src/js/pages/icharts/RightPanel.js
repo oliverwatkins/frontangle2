ad3cbe02f80b2d211b1645ee7b6a811a11ae2f30
watkinso <watkinso@hansaluftbild.de>
2018-01-26 15:42:36 +1100

    maven pre

--- src/js/pages/icharts/RightPanel.js
+++ src/js/pages/icharts/RightPanel.js
@@ -6,36 +6,19 @@ import githubImage from './img/download_github.jpg';
 
 export default class RightPanel extends React.Component {
 
-// .rightpanel {
-//   border: 1px solid black;
-//   //background:white;
-//   padding: 20px;
-//   background: #90793f;
-//   width: 200px;
-//   height: 300px;
-// }
-//
-//   #right-panel {
-//   background-size: 100px 100px;
-//   background-repeat: no-repeat;
-//
-//   //background: #8f9060;
-//   //border-bottom: double #3a4a55 thick;
-//   //size:100px 100px;
-//   background-image: url("img/download_github.jpg");
-//   //width:100px;
-//   height: 100px;
-//
-// }
-
 
   render() {
+    const styleGithubButton = {
+      margin: 15
+
+    }
+
     const styleSection = {
       margin:'0.5em',
-      // border: '5px solid red',
       float:'right',
-      height:'300px',
-      width:'220px'
+      height:'1000',
+      width:'300px',
+      background:'#ebebeb'
     }
 
     const { location } = this.props;
@@ -43,22 +26,27 @@ export default class RightPanel extends React.Component {
     return (
       <section className="container " style={styleSection}>
         <a href="https://github.com/oliverwatkins/Iceberg-Charts">
-          <img src={githubImage}/>
+          <img style={styleGithubButton} src={githubImage}/>
         </a>
+        <div>
+          Add it to your maven project :
+        </div>
+
+        <pre>
+          <span style={{fontSize:10, fontFamily: "courier new"}}>
+            <code>
+              &lt;dependency&gt; <br/>
+              &nbsp; &lt;groupId&gt;com.frontangle&lt;/groupId&gt; <br/>
+              &nbsp; &lt;artifactId&gt;iceberg-charts&lt;/artifactId&gt; <br/>
+              &nbsp; &lt;version&gt;1.1.1&lt;/version&gt; <br/>
+              &lt;/dependency&gt;
+            </code>
+          </span>
+        </pre>
+
+        And quickly get started with <a>these code samples</a>.
+
       </section>
     )
   }
 }
-{/*<div id="navcontainer" class="collapse navbar-collapse navbar-ex1-collapse">*/}
-  {/*<ul>*/}
-    {/*<li class="item" ng-click="click($event, 1)" ng-mouseenter="hoverIn($event)" ng-mouseleave="hoverOut($event)">*/}
-      {/*<a href="#/home">Home</a>*/}
-    {/*</li>*/}
-    {/*<li class="item" ng-click="click($event, 4)" ng-mouseenter="hoverIn($event)" ng-mouseleave="hoverOut($event)">*/}
-      {/*<a href="#/screenshots">Screenshots</a>*/}
-    {/*</li>*/}
-    {/*<li class="item" ng-click="click($event, 2)" ng-mouseenter="hoverIn($event)" ng-mouseleave="hoverOut($event)">*/}
-      {/*<a href="#/codesamples">Code Samples</a>*/}
-    {/*</li>*/}
-  {/*</ul>*/}
-{/*</div>*/}
--- src/js/pages/icharts/Screenshots.js
+++ src/js/pages/icharts/Screenshots.js
@@ -16,10 +16,6 @@ import img9 from './screenshots/img9.PNG'
 import img10 from './screenshots/img10.PNG'
 
 
-// var fs = require('fs');
-// var files = fs.readdirSync('/assets/photos/');
-
-
 /**
  * Contains nav on top, footer on bottom, and all the pages are passed in
  * as children..
@@ -27,67 +23,29 @@ import img10 from './screenshots/img10.PNG'
 export default class PageLayoutICharts extends React.Component {
 
   componentDidMount() {
-    // request
-    //   .get('http://localhost:8080/#/icharts/screenshots/screenshots')
-    //   .then(function(res) {
-    //
-    //     // alert(JSON.stringify(res))
-    //   })
-    //   .catch(function(err) {
-    //     // alert(err)
-    //   });
   }
-
-
-
 
   render() {
     const {location} = this.props;
-    const containerStyle = {
+
+    const style = {
+      width: '300px',
       marginTop: "0px"
-    };
+    }
 
-
-
-    // if (location.pathname.match(/^\/icharts/)) {
     return (
       <div>
-        <img src={img1} className='screenshot-image'/>
-        <img src={img2} className='screenshot-image'/>
-        <img src={img3} className='screenshot-image'/>
-        <img src={img4} className='screenshot-image'/>
-        <img src={img5} className='screenshot-image'/>
-        <img src={img6} className='screenshot-image'/>
-        <img src={img7} className='screenshot-image'/>
-        <img src={img8} className='screenshot-image'/>
-        <img src={img9} className='screenshot-image'/>
-        <img src={img10} className='screenshot-image'/>
-
-
-
-
+        <img src={img1} style={style} className='screenshot-image'/>
+        <img src={img2} style={style} className='screenshot-image'/>
+        <img src={img3} style={style} className='screenshot-image'/>
+        <img src={img4} style={style} className='screenshot-image'/>
+        <img src={img5} style={style} className='screenshot-image'/>
+        <img src={img6} style={style} className='screenshot-image'/>
+        <img src={img7} style={style} className='screenshot-image'/>
+        <img src={img8} style={style} className='screenshot-image'/>
+        <img src={img9} style={style} className='screenshot-image'/>
+        <img src={img10} style={style} className='screenshot-image'/>
       </div>
-
     )
-
-
-    // else {
-    //   return (
-    //     <div>
-    //       <Nav location={location} />
-    //
-    //       <div style={containerStyle}>
-    //         <div>
-    //           <div>
-    //             {this.props.children}
-    //           </div>
-    //         </div>
-    //         <Footer/>
-    //       </div>
-    //     </div>
-    //   );
-    // }
-
-
   }
 }
